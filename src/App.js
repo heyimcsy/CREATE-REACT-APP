e129c0f93718b17f5ef5733db230ef0ff59cffba
최서윤 <cinnamon1995@naver.com>
2023-06-16 16:58:36 +0900

    컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React from 'react'
 import { useState } from 'react'
 import './App.css'
+import Form from './components/Form'
 import List from './components/List'
 
 export default function App() {
   const [todoData, setTodoData] = useState([])
   const [value, setValue] = useState('')
-
-  const getStyle = (completed) => {
-    return {
-      padding: '10px',
-      borderBottom: '1px #ccc dotted',
-      textDecoration: completed ? 'line-through' : 'none',
-    }
-  }
-
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter((data) => data.id !== id)
-    console.log('newTodoData', newTodoData)
-    setTodoData(newTodoData)
-  }
-
-  const handleChange = (event) => {
-    console.log(event.target.value)
-    setValue(event.target.value)
-  }
 
   const handleSubmit = (event) => {
     //form 안에 input 을 전송 할 때 페이지 리로드 되는 것을 막아줌
@@ -41,15 +23,6 @@ export default function App() {
     setTodoData((prev) => [...prev, newTodo])
     setValue('')
   }
-  const handleCompleteChange = (id) => {
-    let newTodoData = todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed
-      }
-      return data
-    })
-    setTodoData(newTodoData)
-  }
 
   return (
     <div className="container">
@@ -57,19 +30,9 @@ export default function App() {
         <div className="title">
           <h1>할 일 목록</h1>
         </div>
-        <List todoData={todoData} />
+        <List todoData={todoData} setTodoData={setTodoData} />
 
-        <form style={{ display: 'flex' }} onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="value"
-            style={{ flex: '10', padding: '5px' }}
-            placeholder="해야 할 일을 입력하세요."
-            value={value}
-            onChange={handleChange}
-          />
-          <input type="submit" value="입력" className="btn" style={{ flex: '1' }} />
-        </form>
+        <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
       </div>
     </div>
   )

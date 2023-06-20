66d7b0bc8560afa893b7ef69c66f8f4adf57e141
최서윤 <cinnamon1995@naver.com>
2023-06-20 18:57:21 +0900

    List 파일 분리

--- src/components/List.js
+++ src/components/List.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 
-export default function List({ todoData, setTodoData }) {
+const List = ({ id, title, completed, todoData, setTodoData, provided, snapshot }) => {
   const handleClick = (id) => {
     let newTodoData = todoData.filter((data) => data.id !== id)
     console.log('newTodoData', newTodoData)
@@ -17,53 +16,27 @@ export default function List({ todoData, setTodoData }) {
     })
     setTodoData(newTodoData)
   }
-  const handleEnd = (result) => {
-    if (!result.destination) return
-
-    const newTodoData = [...todoData]
-
-    const [reoderedItem] = newTodoData.splice(result.source.index, 1)
-
-    newTodoData.splice(result.destination.index, 0, reoderedItem)
-    setTodoData(newTodoData)
-  }
-
   return (
-    <div>
-      <DragDropContext onDragEnd={handleEnd}>
-        <Droppable droppableId="todo">
-          {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              {todoData.map((data, index) => (
-                <Draggable key={data.id} draggableId={data.id.toString()} index={index}>
-                  {(provided, snapshot) => (
-                    <div
-                      key={data.id}
-                      {...provided.draggableProps}
-                      ref={provided.innerRef}
-                      {...provided.dragHandleProps}
-                      className={`${
-                        snapshot.isDragging ? 'bg-gray-400' : 'bg-gray-100'
-                      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}
-                    >
-                      <div className="items-center ">
-                        <input type="checkbox" defaultChecked={false} onChange={() => handleCompleteChange(data.id)} />
-                        <span className={data.completed ? 'line-through' : undefined}>{data.title}</span>
-                      </div>
-                      <div className="items-center ">
-                        <button className="px-4 py-2 float-right" onClick={() => handleClick(data.id)}>
-                          x
-                        </button>
-                      </div>
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+    <div
+      key={id}
+      {...provided.draggableProps}
+      ref={provided.innerRef}
+      {...provided.dragHandleProps}
+      className={`${
+        snapshot.isDragging ? 'bg-gray-400' : 'bg-gray-100'
+      } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounded`}
+    >
+      <div className="items-center ">
+        <input type="checkbox" defaultChecked={false} onChange={() => handleCompleteChange(id)} />
+        <span className={completed ? 'line-through' : undefined}>{title}</span>
+      </div>
+      <div className="items-center ">
+        <button className="px-4 py-2 float-right" onClick={() => handleClick(id)}>
+          x
+        </button>
+      </div>
     </div>
   )
 }
+
+export default List
---
+++ src/components/Lists.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import List from './List'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+
+export default function Lists({ todoData, setTodoData }) {
+  const handleEnd = (result) => {
+    if (!result.destination) return
+
+    const newTodoData = [...todoData]
+
+    const [reoderedItem] = newTodoData.splice(result.source.index, 1)
+
+    newTodoData.splice(result.destination.index, 0, reoderedItem)
+    setTodoData(newTodoData)
+  }
+
+  return (
+    <div>
+      <DragDropContext onDragEnd={handleEnd}>
+        <Droppable droppableId="todo">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {todoData.map((data, index) => (
+                <Draggable key={data.id} draggableId={data.id.toString()} index={index}>
+                  {(provided, snapshot) => (
+                    <List
+                      key={data.id}
+                      id={data.id}
+                      title={data.title}
+                      completed={data.completed}
+                      todoData={todoData}
+                      setTodoData={setTodoData}
+                      provided={provided}
+                      snapshot={snapshot}
+                    />
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  )
+}

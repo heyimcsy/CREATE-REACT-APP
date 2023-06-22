2485492f76e78cf8af3ef864175d316d476da2d8
최서윤 <cinnamon1995@naver.com>
2023-06-22 14:32:10 +0900

    할일 전체 삭제

--- src/App.js
+++ src/App.js
@@ -34,11 +34,15 @@ export default function App() {
     setValue('')
   }
 
+  const handleRemoveClick = () => {
+    setTodoData([])
+  }
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
       <div className="w-full p-6 m-4 bg-white rounded shadow lg:max-w-lg">
         <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
         </div>
         <Lists todoData={todoData} setTodoData={setTodoData} handleClick={handleClick} />
         <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
